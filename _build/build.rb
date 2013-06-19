eb1abe05c4ba32b8e3160fe1c759957fd1443d34
Steven Merrill <steven.merrill@gmail.com>
2013-06-19 03:36:22 -0400

    Le sigh. Let's go back to PHP-FPM.

--- _build/build.rb
+++ _build/build.rb
@@ -22,46 +22,53 @@ extract_directory = script_path + "/rpm-extract"
 # Ensure that the package is actually available for download from IUS.
 # This can probably be done better with native repository tools. C'est la vie.
 def check_package (url)
-  Net::HTTP.start("dl.iuscommunity.org") do |http|
+  Net::HTTP.start('dl.iuscommunity.org') do |http|
     http.open_timeout = 2
     http.read_timeout = 2
-    http.head(url).code == "200"
+    http.head(url).code == '200'
   end
 end
 
 packages = [
-  "php54-cli",
-  "php54-mbstring",
-  "php54-devel",
-  "php54",
-  "php54-gd",
-  "php54-soap",
-  "php54-xml",
-  "php54-pdo",
-  "php54-mysql",
-  "php54-process",
-  "php54-common"
+  'php54-cli',
+  'php54-mbstring',
+  'php54-devel',
+  'php54',
+  'php54-gd',
+  'php54-soap',
+  'php54-xml',
+  'php54-pdo',
+  'php54-mysql',
+  'php54-process',
+  'php54-common',
+  'php54-fpm',
 ]
 
 package_urls = packages.map { |p| format_string % [arch, p, version, arch] }.select { |p| check_package(p) }
-package_urls = package_urls.map { |url| "http://dl.iuscommunity.org%s" % url }
+package_urls = package_urls.map { |url| 'http://dl.iuscommunity.org%s' % url }
 
 # Add extra packages that we have to have in the tarball.
-package_urls << [
-  "http://mirrors.rit.edu/centos/6.4/os/x86_64/Packages/t1lib-5.1.2-6.el6_2.1.x86_64.rpm",
+package_urls.concat [
+  'http://dl.iuscommunity.org/pub/ius/stable/Redhat/6/x86_64/php54-pecl-mongo-1.3.2-1.ius.el6.x86_64.rpm',
+  'http://dl.iuscommunity.org/pub/ius/stable/Redhat/6/x86_64/php54-pecl-apc-3.1.13-1.ius.el6.x86_64.rpm',
+  'http://mirrors.rit.edu/centos/6.4/os/x86_64/Packages/t1lib-5.1.2-6.el6_2.1.x86_64.rpm',
+  'http://apt.sw.be/redhat/el6/en/x86_64/rpmforge/RPMS/mod_fastcgi-2.4.6-2.el6.rf.x86_64.rpm',
 ]
 
 # Download the packages that are not 404s.
 Dir.chdir(download_directory)
 package_urls.each { |url|
   puts "Downloading #{url}."
-  system("wget %s" % url)
+  system('wget %s' % url)
 }
 
 # We have to chdir here, since cpio wants us to be in the directory where we will expand things.
 Dir.chdir(extract_directory)
-Dir.glob('../*/*.rpm').each { |file|
-  rpm_path = download_directory + "/" + file.split("/").last
-  system("rpm2cpio %s | cpio -idv" % rpm_path)
+Dir.glob('%s/*.rpm' % download_directory).each { |file|
+  rpm_path = download_directory + '/' + file.split('/').last
+  system('rpm2cpio %s | cpio -idv' % rpm_path)
 }
 
+# Now, some OpenShifty cleanup.
+# For one, we want the PHP cartridge's php.ini to win.
+FileUtils.rm('%s/etc/php.ini' % extract_directory)
